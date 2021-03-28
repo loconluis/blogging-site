a8d4cf10c91e84a1afa0e0bb2a055d941f451148
Luis Locon <luis.locon@gmail.com>
2021-03-28 13:34:03 -0600

    Deleting console logs

--- app/components/CodeBlock.js
+++ app/components/CodeBlock.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Highlight, { defaultProps } from "prism-react-renderer";
-import theme from 'prism-react-renderer/themes/nightOwl'
-
-console.log('theme', theme)
+import theme from "prism-react-renderer/themes/nightOwl";
 
 const CodeBlock = ({ children, className }) => {
   const language = className.replace(/language-/, "");
 
   return (
-    <Highlight {...defaultProps} code={children.trim()} language={language} theme={theme} >
+    <Highlight
+      {...defaultProps}
+      code={children.trim()}
+      language={language}
+      theme={theme}
+    >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <pre className={className} style={{ ...style, padding: "20px" }}>
           {tokens.map((line, i) => (
