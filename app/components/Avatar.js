cef1b3814080f41139b9328fa2da2825d345bc19
Luis Locon <luis.locon@gmail.com>
2021-03-31 14:30:14 -0600

    Change img element for a Next Component

--- app/components/Avatar.js
+++ app/components/Avatar.js
@@ -1,14 +1,17 @@
 import Link from "next/link";
+import Image from 'next/image'
 
 export default function Avatar({ size }) {
   return (
     <Link href="/">
       <a>
-        <img
+        <Image
           width={size == "small" ? 50 : 75}
           height={size == "small" ? 50 : 75}
           className="sidebar-main-avatar"
           src="/images/me.png"
+          layout="intrinsic"
+          loading="eager"
         />
       </a>
     </Link>
