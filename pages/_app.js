b986d77905585e7397ee841df7b62cb24e01c37a
Luis Locon <luis.locon@gmail.com>
2021-04-06 23:31:05 -0600

    Dark Mode (#3)
    * new entry
    
    * Fixing About Me Page
    
    * fixes optimize image
    
    * Feature/dark mode (#2)
    
    * adding stylesheet to darkmode
    
    * adding darkmode

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,35 @@
 import { DefaultSeo } from "next-seo";
-
+import { useState, useEffect } from "react";
+import ThemeContext from "../app/context/Theme";
 import SEO from "../next-seo.config";
 import "../styles/style.css";
 
 function MyApp({ Component, pageProps }) {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    function loadTheme() {
+      document.addEventListener("DOMContentLoaded", (event) => {
+        const current = localStorage.getItem("currentThemeSelected");
+        document.documentElement.setAttribute("data-theme", current);
+        setTheme(current);
+      });
+    }
+
+    loadTheme();
+
+    function cleanUp() {
+      window.removeEventListener("DOMContentLoaded", () => {});
+    }
+    return cleanUp();
+  }, []);
   return (
-    <div className="holi">
-      <DefaultSeo {...SEO} />
-      <Component {...pageProps} />;
-    </div>
+    <>
+      <ThemeContext.Provider value={theme}>
+        <DefaultSeo {...SEO} />
+        <Component {...pageProps} />
+      </ThemeContext.Provider>
+    </>
   );
 }
 
