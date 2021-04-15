03277fde074d69374ffa1e36903951d19492fc6f
Luis Locon <luis.locon@gmail.com>
2021-04-14 20:34:29 -0600

    Supporting a counter of views (#6)
    * Supporting a counter of views
    
    * Fixing text when is singular

--- app/components/Info.js
+++ app/components/Info.js
@@ -1,17 +1,23 @@
-import Avatar from "./Avatar";
-import Social from "./Social";
-import { parseISO, format } from 'date-fns'
+import Avatar from './Avatar';
+import Social from './Social';
+import { formatNumberWithCommas } from '../../lib/formatCommasNumber';
+import { parseISO, format } from 'date-fns';
 
-export default function Info({ frontMatter }) {
+export default function Info({ frontMatter, views }) {
   return (
     <div className="blog-header">
       <h1 className="blog-title">{frontMatter.title}</h1>
       <div className="blog-info">
         <div className="blog-info-data">
-          <Avatar size={"small"} />
+          <Avatar size={'small'} />
           <p className="blog-info-data-author">{frontMatter.author}</p>
-          <p className="blog-info-data-date">{format(parseISO(frontMatter.publishedAt), 'MMMM dd, yyyy')}</p>
-          <Social info/>
+          <p className="blog-info-data-date">
+            {format(parseISO(frontMatter.publishedAt), 'MMMM dd, yyyy')}
+          </p>
+          <Social info />
+          <p className={`blog-info-data-views ${!views ? 'views-loading' : ''}`}>
+            {formatNumberWithCommas(views)} {views == 1 ? "view" : "views"}
+          </p>
         </div>
         <p className="blog-info-time">{frontMatter.readingTime.text}</p>
       </div>
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,31 +1,35 @@
-import hydrate from "next-mdx-remote/hydrate";
-import { getFiles, getFileBySlug } from "../../lib/mdx";
-import Head from "next/head";
-import BlogLayout from "../../app/Layout/Blog";
-import MDXComponents from "../../app/components/MDXComponents";
+import hydrate from 'next-mdx-remote/hydrate';
+import { getFiles, getFileBySlug } from '../../lib/mdx';
+import Head from 'next/head';
+import BlogLayout from '../../app/Layout/Blog';
+import MDXComponents from '../../app/components/MDXComponents';
+import withViews from '../../lib/useViews';
 
 export default function BlogPage({ mdxSource, frontMatter }) {
   const content = hydrate(mdxSource, {
     components: MDXComponents,
   });
 
+  const { views } = withViews();
+  console.log('view', views)
+
   return (
     <>
       <Head>
         <title>{frontMatter.title} - Blog by Luis Locon</title>
       </Head>
-      <BlogLayout frontMatter={frontMatter}>{content}</BlogLayout>
+      <BlogLayout views={views} frontMatter={frontMatter}>{content}</BlogLayout>
     </>
   );
 }
 
 export async function getStaticPaths() {
-  const posts = await getFiles("/");
+  const posts = await getFiles('/');
 
   return {
     paths: posts.map((p) => ({
       params: {
-        slug: p.replace(/\.mdx/, ""),
+        slug: p.replace(/\.mdx/, ''),
       },
     })),
     fallback: false,
@@ -33,6 +37,6 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const post = await getFileBySlug("/", params.slug);
+  const post = await getFileBySlug('/', params.slug);
   return { props: post };
 }
