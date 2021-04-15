9ac8a75c8f5203cdde453eee191d3271b14ca00e
Luis Locon <luis.locon@gmail.com>
2021-04-14 20:36:04 -0600

    Counter view (#7)
    * new entry
    
    * Fixing About Me Page
    
    * fixes optimize image
    
    * Feature/dark mode (#2)
    
    * adding stylesheet to darkmode
    
    * adding darkmode
    
    * Feature/dark mode (#4)
    
    * adding stylesheet to darkmode
    
    * adding darkmode
    
    * Fix dark mode
    
    * Supporting a counter of views (#6)
    
    * Supporting a counter of views
    
    * Fixing text when is singular

--- app/Layout/Blog.js
+++ app/Layout/Blog.js
@@ -2,11 +2,11 @@ import Info from "../components/Info";
 import NavbarBlog from "../components/NavbarBlog";
 import ThemeSwitcher from "../components/ThemeSwitcher";
 
-export default function Blog({ frontMatter, children }) {
+export default function Blog({ frontMatter, views, children }) {
   return (
     <div className="blog">
       <NavbarBlog />
-      <Info frontMatter={frontMatter} />
+      <Info frontMatter={frontMatter} views={views} />
       <div className="blog-container">
         <ThemeSwitcher />
         {children}
