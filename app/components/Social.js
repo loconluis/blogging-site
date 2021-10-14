1f5ddb0eddcfaa8fc9e858f799fbcee51af42eec
Luis Locon <luis.locon@gmail.com>
2021-10-13 23:22:04 -0600

    Adding a new social icon for Resume

--- app/components/Social.js
+++ app/components/Social.js
@@ -1,22 +1,27 @@
-import { FaLinkedinIn, FaGithub, FaTwitter } from "react-icons/fa";
+import { FaLinkedinIn, FaGithub, FaTwitter, FaRegFileAlt } from "react-icons/fa";
 
 export default function Social({ info }) {
   return (
     <div className={info ? "sidebar-main-social__info" : "sidebar-main-social"}>
       <ul className="sidebar-main-social-list">
         <li className="sidebar-main-social-list-item">
-          <a href="https://www.linkedin.com/in/loconluis/" target="_blank">
+          <a href="https://www.linkedin.com/in/loconluis/" target="_blank" aria-label="LinkedIn">
             <FaLinkedinIn />
           </a>
         </li>
         <li className="sidebar-main-social-list-item">
-          <a href="https://github.com/loconluis/" target="_blank">
+          <a href="https://github.com/loconluis/" target="_blank" aria-label="Github">
             <FaGithub />
           </a>
         </li>
-        <li className="sidebar-main-social-list-item">
+        <li className="sidebar-main-social-list-item" aria-label="Twitter">
           <a href="https://twitter.com/LoconLuis" target="_blank">
             <FaTwitter />
+          </a>
+        </li>
+        <li className="sidebar-main-social-list-item">
+          <a href="https://read.cv/loconluis" target="_blank" aria-label="Resume">
+            <FaRegFileAlt />
           </a>
         </li>
       </ul>
