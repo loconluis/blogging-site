f3b2dd1925845c4c65c3e0de956d1058c868e20a
Luis Locon <luis.locon@gmail.com>
2022-11-06 14:02:57 -0600

    Update Contact.js

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
         Contact Me
       </h1>
       <p>
-        Check the buttons in the sidebar or send me an email <a href="mailto:luis.locon@gmail.com">luis.locon@gmail.com</a>
+        Check the buttons in the sidebar
       </p>
     </>
   )
